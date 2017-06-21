22b8d6b5e22d62861a2dd7c8906126a3c66f8f36
May <May@MAY-PC>
2017-06-21 10:12:15 +0300

    ...

--- WebServerAndClient/WebServerAndClient/Models/Client.cs
+++ WebServerAndClient/WebServerAndClient/Models/Client.cs
@@ -7,7 +7,7 @@ namespace WebServerAndClient.Models
 {
     public class Client
     {
-        public int ID { get; set; }
+        public int ID { get; set; } //
         public string UserName { get; set; }
         public string Passward { get; set; }
         public string EMail { get; set; }
